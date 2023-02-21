620af59e047ec3b4cbadbb1a1ed8de7fb36aa92d
KhotsoSelading <k.selading007@gmail.com>
2023-02-21 03:53:55 -0800

    All 0x02 Tasks

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * main - computes the sum of all
+ *	the multiples of 3 or 5
+ *	below 1024 (excluded)
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int sum;
+        int num;
+
+	for (num = 0; num < 1024; ++num)
+	{
+		if ((num % 3 == 0) || (num % 5 == 0))
+		{
+			sum = sum + num;
+		}
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
