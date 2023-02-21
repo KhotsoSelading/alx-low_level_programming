bc2037b92dd47ce2fbdd1f1176e56e82c690174c
KhotsoSelading <k.selading007@gmail.com>
2023-02-21 02:38:03 -0800

    All 0x02 Tasks

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,12 +11,12 @@
 
 void times_table(void)
 {
-	int prod;
+	int prod, num, mult;
 
-	for (int num = 0; num <= 9; ++num)
+	for (num = 0; num <= 9; ++num)
 	{
 		_putchar(48);
-		for (int mult = 1; mult <= 9; ++mult)
+		for (mult = 1; mult <= 9; ++mult)
 		{
 			_putchar(',');
 			_putchar(' ');
