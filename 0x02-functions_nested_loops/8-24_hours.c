02dd846b113fd9cadee73d59cba3638049c2eac2
KhotsoSelading <k.selading007@gmail.com>
2023-02-21 02:35:44 -0800

    All 0x02 Tasks

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,14 +1,16 @@
-#include"main.h"
+#include "main.h"
 
 /**
- * jack_bauer- prints every minute of the day
+ * jack_bauer - prints every minute of the day.
 */
 
 void jack_bauer(void)
 {
-	for (int hr = 0; hr <= 23; ++hr)
+	int min, hr;
+
+	for (hr = 0; hr <= 23; ++hr)
 	{
-		for (int min = 0; min <= 59; ++min)
+		for (min = 0; min <= 59; ++min)
 		{
 			_putchar((hr / 10) + 48);
 			_putchar((hr % 10) + 48);
