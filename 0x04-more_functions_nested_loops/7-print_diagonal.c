8be41a2f2c3c1eeac4127b6db49ad4ed1f7bcdbe
KhotsoSelading <k.selading007@gmail.com>
2023-02-23 09:31:46 -0800

    All 0x04 Tasks

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,13 +11,15 @@ void print_diagonal(int n)
 {
 	int iPos, iSpace;
 
-	if (n <= 0)
-		_putchar('\n');
 	for (iPos = 1; iPos <= n; ++iPos)
 	{
 		for (iSpace = 1; iSpace <= iPos; ++iSpace)
 			_putchar(' ');
 		_putchar(92);
+		_putchar('\n');
+	}
+	if (n <= 0)
+	{
 		_putchar('\n');
 	}
 }
