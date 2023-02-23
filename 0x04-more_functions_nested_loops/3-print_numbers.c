0022eafc48de12792ea9cff3ce6004dcd8d35408
KhotsoSelading <k.selading007@gmail.com>
2023-02-23 05:19:29 -0800

    0x04 tasks

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - print 0 - 9
+ * 		   only using _putchar twice.
+ *
+ * Return: Always 0. (Success)
+*/
+
+void print_numbers(void)
+{
+	int iNum = 0;
+
+	do
+	{
+		_putchar(iNum + 48);
+		iNum++;
+	} while ((iNum >= 0) && (iNum <= 9));
+	_putchar('\n');
+}
