d7a9912f7d28c02fe38a9be0d636aef26d0c63c3
KhotsoSelading <k.selading007@gmail.com>
2023-02-22 22:44:00 -0800

    All 0x03-debugging tasks

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* main - uses function to prints if integer is positive or negative
+* Return: 0. (Success)
+*/
+
+int main(void)
+{
+	int i;
+
+	i = 98 % 2;
+	positive_or_negative(i);
+
+	return (0);
+}
