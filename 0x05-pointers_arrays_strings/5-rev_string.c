8dfa3c3d5503cebe40ece6de6d2c99218ba7d62a
KhotsoSelading <k.selading007@gmail.com>
2023-02-27 05:49:18 -0800

    All 0x05 Tasks

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ *
+ * @s: string parameter inputted
+ *
+ * Return: Nothing
+*/
+
+void rev_string(char *s)
+{
+	char ch;
+	int iLen, iCounter;
+
+	for (iLen = 0; s[iLen] != '\0'; ++iLen)
+		; /*find string length without null charater*/
+
+	/*reverse the string*/
+	for (iCounter = 0; iCounter < iLen / 2; ++iCounter)
+	{
+		ch = s[iCounter];
+		s[i] = s[iLen - 1 - iCounter]; 
+		s[iLen - 1 - i] = ch;
+	}
+
+}
