08e3211de5599546c5335eca275c6d20d4ab2577
KhotsoSelading <k.selading007@gmail.com>
2023-02-27 02:36:08 -0800

    All 0x05 Tasks

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+/**
+ * _strlen - returns length of a string
+ *
+ * @s: string parameter inputted
+ *
+ * Return: length of string
+*/
+
+int _strlen(char *s)
+{
+	int x; /*counter variable*/
+
+	for (x = 0; *s != '\0'; ++s)
+		++counter;
+
+	return (counter);
+}
