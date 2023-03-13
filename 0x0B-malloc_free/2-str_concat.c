63bc24d691d10e66ac4871cb137d253bdc2ce807
KhotsoSelading <k.selading007@gmail.com>
2023-03-13 04:34:47 -0700

    All 0x0B Tasks

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -19,11 +19,8 @@ char *str_concat(char *s1, char *s2)
 	if (s2 == NULL)
 		s2 = "";
 
-	while (s1[i] != '\0' && s2[j] != '\0')
-	{
-		i++;
-		j++;
-	}
+	for (i = 0, j = 0; s1[i] != '\0' && s2[j] != '\0'; i++, j++)
+		;
 
 	sOutput = malloc(sizeof(char) * (i + j + 1));
 
