fc4d1cf51fdd776b771c74bd871c1a4afaa4ee6d
KhotsoSelading <k.selading007@gmail.com>
2023-03-17 06:01:04 -0700

    All 0x0C Tasks

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -24,7 +24,13 @@ int main(int argc, char *argv[])
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			if (argv[i][j] < 48 && argv[i][j] > 57)
+			if (argv[i][j] < 48)
+			{
+				printf("Error\n");
+				exit(98);
+			}
+
+			if (argv[i][j] > 57)
 			{
 				printf("Error\n");
 				exit(98);
