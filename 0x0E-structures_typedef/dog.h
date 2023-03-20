6b2cf08e34bf293843c379e5080bd5468727ae7b
KhotsoSelading <k.selading007@gmail.com>
2023-03-20 05:28:23 -0700

    All 0x0E Tasks

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -4,12 +4,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/**
-* dog_t - Typedef for struct dog
-**/
 
+/**
+ * dog_t - Typedef for struct dog
+ */
 typedef struct dog dog_t;
 
+/**
+ * struct dog - struct that stores some information of a dog
+ *
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
 struct dog
 {
 	char *name;
