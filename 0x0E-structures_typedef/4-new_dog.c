5dd9d46031e0e8ef368cce53522d9c997f5cd188
KhotsoSelading <k.selading007@gmail.com>
2023-03-20 05:38:26 -0700

    All 0x0E Tasks

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -17,11 +17,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 	int i = 0, j = 0, k;
 	dog_t *d_izinja;
 
-	for (i = 0; name[i] != '\0'; i++)
-		;
-
-	for (j = 0; owner[j] != '\0'; j++)
-		;
+	while (name[i] != '\0')
+		i++;
+	while (owner[j] != '\0')
+		j++;
 
 	d_izinja = malloc(sizeof(dog_t));
 
