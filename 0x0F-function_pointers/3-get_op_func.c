6ef90460f72614754e6c88d82d65b8519a02b92a
KhotsoSelading <k.selading007@gmail.com>
2023-03-22 07:41:29 -0700

    All 0x0F Tasks

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -21,12 +21,12 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i = 0;
 
-	while (i < 5)
+	while (i < 10)
 	{
-		if (*s == *ops[i].op)
-			return (ops[i].f);
+		if (s[0] == ops->op[i])
+			break;
 		i++;
 	}
 
-	return (NULL);
+	return (ops[i / 2].f);
 }
