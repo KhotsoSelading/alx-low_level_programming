17e6be96909453d8a3834ae0536887ff307bb2fc
KhotsoSelading <k.selading007@gmail.com>
2023-03-22 10:26:57 -0700

    All 0x0F Tasks

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -20,10 +20,10 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	if (argv[2][1])
+	if ((argv[2][0]) && (y == 0))
 	{
 		printf("Error\n");
-		exit(99);
+		exit(100);
 	}
 
 	operation = get_op_func(argv[2]);
