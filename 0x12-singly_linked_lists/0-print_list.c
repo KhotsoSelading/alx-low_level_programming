2d55a66564ebf82c0a748088bdf5ec14932ea970
KhotsoSelading <k.selading007@gmail.com>
2023-03-30 04:29:40 -0700

    All 0x12 Tasks

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,24 +10,16 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t node_count = 1;
-
-	if (h == NULL)
-		return (0);
-
-	while (h->next != NULL)
+	size_t numElements = 0;
+	
+	while (h != NULL)
 	{
 		if (h->str == NULL)
 			printf("[%d] %s\n", 0, "(nil)");
 		else
 			printf("[%d] %s\n", h->len, h->str);
-
 		h = h->next;
-
-		node_count += 1;
+		numElements++;
 	}
-
-	printf("[%d] %s\n", h->len, h->str);
-
-	return (node_count);
+	return (numElements);
 }
