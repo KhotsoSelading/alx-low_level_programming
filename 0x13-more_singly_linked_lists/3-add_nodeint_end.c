0d89b618a725481a4ceeded0610908ebffc61dbc
KhotsoSelading <k.selading007@gmail.com>
2023-04-03 11:18:59 -0700

    All 0x13 Tasks

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,21 +1,18 @@
 #include "lists.h"
 
 /**
- * add_nodeint_end - function that adds a new node
- *            at the end of the list
+ * add_nodeint_end - add a new node at the end of linked list
  *
- * @head: points to the first node of the list
- * @n: int element to add to new node
+ * @head: head of a list.
+ * @n: n element.
  *
- * Return: address of the new element or NULL if it fails
-*/
-
+ * Return: address of the new element. NUll if it failed.
+ */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint_t *new,  *iHolder;
+	listint_t *new, *holder;
 
-	(void)iHolder;
-
+	(void)holder;
 	new = malloc(sizeof(listint_t));
 
 	if (new == NULL)
@@ -23,20 +20,20 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 
 	new->n = n;
 	new->next = NULL;
-	iHolder = *head;
+	holder = *head;
 
-	/* if head is NULL, place new node*/
-	/* if head is not null, next is not null; therefore place new node at end*/
-	if (*head == NULL)
+	if (*head != NULL)
 	{
-		*head = new;
+		while (holder->next != NULL)
+		{
+			holder = holder->next;
+		}
+		holder->next = new;
+		
 	}
 	else
 	{
-		do {
-			iHolder = iHolder->next;
-		} while (iHolder->next != NULL);
-		iHolder->next = new;
+		*head = new;
 	}
 
 	return (*head);
