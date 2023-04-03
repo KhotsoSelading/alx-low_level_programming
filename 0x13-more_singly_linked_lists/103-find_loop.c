dab9227e9713a1c7778edd890a583751eda91837
KhotsoSelading <k.selading007@gmail.com>
2023-04-03 12:13:32 -0700

    All 0x13 Tasks

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -3,14 +3,14 @@
 /**
  * find_listint_loop - finds the loop in the linked list
  *
- * @head: head of linked list
+ * @head: head of the linked list
  *
  * Return: the address of the node where the loop starts.
  */
 
 listint_t *find_listint_loop(listint_t *head)
 {
-	listint_t *ptr2 = head, *oldPtr = head;
+	listint_t *ptr2 = head, *previous = head;
 
 	do {
 		head = head->next;
@@ -18,23 +18,20 @@ listint_t *find_listint_loop(listint_t *head)
 
 		if (head == ptr2)
 		{
-			head = oldPtr;
-			oldPtr =  ptr2;
-
+			head = previous;
+			previous =  ptr2;
 			while (1)
 			{
-				ptr2 = oldPtr;
-
-				while ((ptr2->next != head) && (ptr2->next != oldPtr))
-
+				ptr2 = previous;
+				while ((ptr2->next != head) && (ptr2->next != previous))
+				{
 					ptr2 = ptr2->next;
-
+				}
 				if (ptr2->next == head)
 					break;
 
 				head = head->next;
 			}
-
 			return (ptr2->next);
 		}
 	} while (head && ptr2 && ptr2->next);
