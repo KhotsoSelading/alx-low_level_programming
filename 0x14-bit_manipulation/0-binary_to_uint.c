8a007dcdfcbe8f073e0b35a84c1e05f616c1b475
KhotsoSelading <k.selading007@gmail.com>
2023-04-09 16:29:59 -0700

    All 0x14 Tasks

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,20 +11,18 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int iBase;
+	unsigned int iBase = 0;
 	int i;
 
 	if (!b)
 		return (0);
 
-	iBase = 0;
-
 	for (i = 0; b[i]; i++)
 	{
-		if ((b[i] < '0') || (b[i] > '1'))
-		{
+		if (b[i] < '0')
 			return (0);
-		}
+		if (b[i] > '1')
+			return (0);
 		iBase = 2 * iBase + (b[i] - '0');
 	}
 
