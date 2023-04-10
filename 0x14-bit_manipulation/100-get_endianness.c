23ce8c547129de756267c769db510d4fe61f673a
KhotsoSelading <k.selading007@gmail.com>
2023-04-10 11:20:19 -0700

    All 0x15 Tasks

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -4,12 +4,13 @@
  * get_endianness - checks if architecture is little endian
  *                  or big endian
  *
- * Return: 0 if big endian. 1 if little endian. 
+ * Return: 0 if big endian. 1 if little endian.
 */
 
 int get_endianness(void)
 {
 	unsigned int end_num = 1;
+
 	char *c = (char *) &end_num;
 	return (c[0]);
 }
