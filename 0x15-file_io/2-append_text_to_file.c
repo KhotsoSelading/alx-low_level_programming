9bd66446cbd29d92c8bb573e1dc8ec06856c3e47
KhotsoSelading <k.selading007@gmail.com>
2023-04-10 03:51:57 -0700

    All 0x15 Tasks

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -18,7 +18,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 		for (iLength = 0; text_content[iLength];)
-			len++;
+			iLength++;
 
 	iOpen = open(filename, O_WRONLY | O_APPEND);
 	iWrite = write(iOpen, text_content, iLength);
