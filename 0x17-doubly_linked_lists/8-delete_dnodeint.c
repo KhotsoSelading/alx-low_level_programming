8433715fd31586f75de6f8f33831e5ba795cd8cb
KhotsoSelading <k.selading007@gmail.com>
2023-06-15 23:18:26 +0200

    0x17 Tasks

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -12,25 +12,32 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	dlistint_t *tmp = *head;
 
-	if (*head == NULL)
+	if (!(*head))
 		return (-1);
 
-	for (unsigned int i = 0; i < index; i++)
+	while (index != 0)
 	{
-		if (tmp == NULL)
+		if (!tmp)
 			return (-1);
 		tmp = tmp->next;
+		index--;
 	}
 
 	if (tmp == *head)
+	{
 		*head = tmp->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
+	}
 	else
+	{
 		tmp->prev->next = tmp->next;
-
-	if (tmp->next != NULL)
-		tmp->next->prev = tmp->prev;
+		if (tmp->next != NULL)
+			tmp->next->prev = tmp->prev;
+	}
 
 	free(tmp);
 	return (1);
 }
+
 
