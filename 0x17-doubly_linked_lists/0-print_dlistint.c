da3503d8d8e10827d9a4a97333b38a0d28fc44ff
KhotsoSelading <k.selading007@gmail.com>
2023-06-15 21:20:28 +0200

    0x17 Tasks

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,14 +9,12 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t count = 0;
-	const dlistint_t *current = h;
-
-	while (current != NULL)
+	
+	for (; h != NULL; h = h->next)
 	{
-		printf("%d\n", current->n);
+		printf("%d\n", h->n);
 		count++;
-		current = current->next;
 	}
-
+	
 	return count;
 }
