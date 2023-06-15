6b51ad4ffd3a3b2f0f60fdc4816f4f1cf1ea1c64
KhotsoSelading <k.selading007@gmail.com>
2023-06-15 21:41:54 +0200

    0x17 tasks

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -23,7 +23,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	{
 		new->prev = NULL;
 		*head = new;
-		return new;
+		return (new);
 	}
 
 	for (current = *head; current->next != NULL; current = current->next)
