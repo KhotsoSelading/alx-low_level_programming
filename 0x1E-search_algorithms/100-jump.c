9c179dac9ed4da5345383886f97100485e5c320c
KhotsoSelading <k.selading007@gmail.com>
2023-10-11 05:13:13 +0200

    0x1E

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -13,24 +13,20 @@
  */
 int jump_search(int *array, size_t size, int value)
 {
-	int m = sqrt(size), k = 0, prev = 0, index = 0;
+	int index = 0, m = (int)sqrt((double)size), k = 0, prev = 0;
 
 	if (array == NULL || size == 0)
 		return (-1);
 
-	while (index < (int)size)
+	while (index < (int)size && array[index] < value)
 	{
 		printf("Value checked array[%d] = [%d]\n", index, array[index]);
 
 		if (array[index] == value)
 			return (index);
-
 		k++;
 		prev = index;
 		index = k * m;
-
-		if (index >= (int)size)
-			break;
 	}
 
 	printf("Value found between indexes [%d] and [%d]\n", prev, index);
